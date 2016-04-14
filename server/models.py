d13beb1bdd1237165fa9349dea41aae0507c4c77
jnwcki <jude.nowicki@gmail.com>
2016-04-13 22:14:44 -0400

    added tax field to restaurant

--- server/models.py
+++ server/models.py
@@ -13,6 +13,7 @@ ITEM_CHOICES = [
 class Restaurant(models.Model):
     name = models.CharField(max_length=255, default="Unnamed Restaurant")
     number_of_tables = models.IntegerField(default=1)
+    tax_percentage = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
 
     def __str__(self):
         return "{}".format(self.name)
