dba4371f1ad76ed7fc4bcf6ae22f23bd6e224574
jnwcki <jude.nowicki@gmail.com>
2016-04-20 12:10:05 -0400

    ran collectstatic

--- server_buddy/settings.py
+++ server_buddy/settings.py
@@ -109,6 +109,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'collect_static')
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.dirname(os.path.join(BASE_DIR, 'media'))
 MEDIA_URL = '/media/'
