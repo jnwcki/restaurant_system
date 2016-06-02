e5720a5d2fe234445c62c2be859c14189bab4801
Jude Nowicki <jude.nowicki@gmail.com>
2016-06-01 22:13:14 -0400

    switched to local db

--- server_buddy/settings.py
+++ server_buddy/settings.py
@@ -67,12 +67,12 @@ WSGI_APPLICATION = 'server_buddy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # DATABASES = {
 #     'default': {
